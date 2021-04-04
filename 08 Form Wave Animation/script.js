1e6be8c9d360b137420fed65b387740a96244dac
MelodyGhost <mohammad.abdurrahim94@gmail.com>
2021-04-04 18:22:55 +0600

    Delete form wave animation

--- 08 Form Wave Animation/script.js
+++
@@ -1,8 +0,0 @@
-let labels = document.querySelectorAll('.label')
-
-labels.forEach(label => {
-    label.innerHTML = label.innerText
-    .split('')
-    .map((char, idx) => `<span style= 'transition-delay:${idx * 50}ms'>${char}</span>`)
-    .join('')
-});
